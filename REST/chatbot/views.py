2ec4655a00e3592b304e4f5ff2fd607f0e3372a3
Omar Abdulle <93823797+siroaa7@users.noreply.github.com>
2024-04-13 15:07:45 +0200

    Update views.py

--- REST/chatbot/views.py
+++ REST/chatbot/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import Feedback
 from ..db_connection import collection
+from AI_Chatbot.Chatbot import model
 
 # Create your views here.
 
@@ -14,7 +15,6 @@ class FeedbackAPI(APIView):
             question = serializer.validated_data['question']
             response = serializer.validated_data['response']
             feedback = serializer.validated_data.get('feedback')
-
             # Save data to MongoDB collection
             collection.insert_one({
                 'question': question,
@@ -27,3 +27,13 @@ class FeedbackAPI(APIView):
             return Response(serializer.errors, status=400)
 
 # Test 2
+
+
+class chatbotView(APIView):
+    def post(self, request):
+        user_input = request.data.get('text')
+        # Processing user input with Llama2
+        response_text = model.predict(user_input)
+        # API response
+        data = {'response': response_text}
+        return Response(data)
