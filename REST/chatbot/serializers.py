e20fbef60e08fc7f40fd12e3e8ac74048a5870ea
Holtwoods <118753570+Holtwoods@users.noreply.github.com>
2024-04-18 17:48:04 +0200

    Removed unnecessary feedback code
    Removed unused code for feedback function due to change of solution

--- REST/chatbot/serializers.py
+++ REST/chatbot/serializers.py
@@ -1,7 +1,3 @@
 from rest_framework import serializers
 
 
-class Feedback(serializers.Serializer):
-    question = serializers.CharField()
-    response = serializers.CharField()
-    feedback = serializers.CharField()
--- REST/chatbot/urls.py
+++ REST/chatbot/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
-from .views import FeedbackAPI
 from .views import ChatbotView
 
 urlpatterns = [
-    path('feedback/', FeedbackAPI.as_view()),
     path('chatbot/', ChatbotView.as_view())
 ]
