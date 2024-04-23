eacbd73549cd918da0d95ceb5bae777ddb26cf2a
Omar Abdulle <93823797+siroaa7@users.noreply.github.com>
2024-04-23 14:40:20 +0200

    Moved back everything

--- REST/chatbot/views.py
+++ REST/chatbot/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from AI_Chatbot.Chatbot import model_dir, get_response
+from .Chatbot import model_dir, get_response
 
 # Create your views here.
 
